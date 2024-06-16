ce5f447be0a9399fa081395a799894f63a24ec6c
GuanYu914 <aa774281@gmail.com>
2024-06-16 14:53:40 +0800

    add custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#e6b57b",
+      secondary: "#dadada",
+      text: "#383838",
+      white: "#ffffff",
+    },
   },
   plugins: [],
 };
