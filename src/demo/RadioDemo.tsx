ba6d578c5f16afa914728ab3d891a74e634a561a
GuanYu <aa74281@gmail.com>
2024-09-13 15:15:43 +0800

    update demo codes for new feature

--- src/demo/RadioDemo.tsx
+++ src/demo/RadioDemo.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Radio } from "@/components";
+import { Radio, RadioGroup } from "@/components";
 
 function RadioDemo() {
+  const [value, setValue] = useState("1");
+
   return (
     <>
-      <Radio onChange={(c) => console.log("checked", c)}>default</Radio>
+      {/* Radio Group*/}
+      <RadioGroup value={value} onChange={(v) => setValue(v)}>
+        <Radio value="1">Option 1</Radio>
+        <Radio value="2" checked>
+          Option 2
+        </Radio>
+        <Radio value="3">Option 3</Radio>
+      </RadioGroup>
+      <br />
+      {/* Radio */}
+      <Radio onChange={(v) => console.log("checked", v)}>default</Radio>
       <Radio checked>checked!</Radio>
       <Radio disabled>disabled</Radio>
       <Radio disabled checked>
