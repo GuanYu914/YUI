b585cbfe2479a62a3f10d7c4e30553183e4d9c06
GuanYu914 <aa74281@gmail.com>
2024-11-14 22:13:29 +0800

    feat: export added components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Button } from "./Button";
 export { default as Switch } from "./Switch";
-export { default as Radio } from "./Radio";
-export { RadioGroup } from "./Radio";
+export { default as Radio, RadioGroup } from "./Radio";
+export { default as Checkbox, CheckboxGroup } from "./Checkbox";
